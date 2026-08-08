c51691cfd42fbc38b0f904b78f4efe101743557b
geysapoisl <geysax@gmail.com>
2026-08-08 20:53:47 -0300

    Create dados.js

---
+++ js/dados.js
@@ -0,0 +1,16 @@
+/* =========================================================
+   BASE DE DADOS - DASHBOARD GSP 2.0
+   Dados organizados por período
+   ========================================================= */
+
+const dadosDashboard = {
+    "2026-05": {
+        mes: "Maio",
+        ano: 2026
+    },
+
+    "2026-06": {
+        mes: "Junho",
+        ano: 2026
+    }
+};
